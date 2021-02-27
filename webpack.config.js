aa777805aaa7978a48a703c593fa78a29039f244
Pavel Karpenko <ospkiev@gmail.com>
2021-02-27 08:31:24 +0200

    wp5

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,13 @@
+const { resolve } = require('path');
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const isProd = process.env.NODE_ENV === 'production';
-const isDev = !isProd;
-
-const fileName = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    filename: fileName('js'),
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: './index.js',
     },
-    extensions: ['.js'],
-  },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
-    port: 3000,
-    hot: isDev,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd,
-      }
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/favicon.ico'), 
-          to: path.resolve(__dirname, 'dist'),
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: fileName('css'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ],
-  },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+    },
 };
