39f156ffd392507068e82d79b4794b6fd9ffeed4
Pavel Karpenko <ospkiev@gmail.com>
2021-03-05 13:07:52 +0200

    eslint added

--- src/module.js
+++ src/module.js
@@ -1,12 +1,14 @@
 function sleep(time = 1000) {
-  return new Promise(resolve => {
-    setTimeout(() => {resolve()}, time);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, time);
   });
-};
+}
 async function start() {
   console.log('1');
   await sleep(3000);
   console.log('2');
-};
+}
 
 start();
